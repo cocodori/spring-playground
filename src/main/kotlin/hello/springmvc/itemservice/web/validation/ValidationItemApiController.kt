3abddd02e8882dcacd5f67006ab9cb261ee83d98
cocodori <moradulee@gmail.com>
2022-08-15 21:45:48 +0900

    HTTPMessageConverter Validation

---
+++ src/main/kotlin/hello/springmvc/itemservice/web/validation/ValidationItemApiController.kt
@@ -0,0 +1,34 @@
+package hello.springmvc.itemservice.web.validation
+
+import hello.springmvc.itemservice.web.validation.form.ItemSaveForm
+import org.slf4j.LoggerFactory
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/validation/api/items")
+class ValidationItemApiController {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @PostMapping("/add")
+    fun addItem(
+        @RequestBody @Validated form: ItemSaveForm,
+        bindingResult: BindingResult,
+    ): Any {
+        log.info("API 컨트롤러 호출")
+
+        if (bindingResult.hasErrors()) {
+            log.info("검증 오류 발생 errors={}", bindingResult)
+            return bindingResult.allErrors
+        }
+
+        log.info("성공 로직")
+
+        return form
+    }
+}
