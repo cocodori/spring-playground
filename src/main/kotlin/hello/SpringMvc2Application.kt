7425edf81588db1ea7db815d9f7ab24c21d645fa
cocodori <moradulee@gmail.com>
2022-09-13 23:46:08 +0900

    고급편 V0

--- src/main/kotlin/hello/SpringMvc2Application.kt
+++ src/main/kotlin/hello/SpringMvc2Application.kt
@@ -1,9 +1,11 @@
-package hello.springmvc
+package hello
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["hello.advanced"])
 class SpringMvc2Application
 
 fun main(args: Array<String>) {
